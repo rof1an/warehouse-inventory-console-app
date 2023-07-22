7e0890efce52c506449d14dbe106d30933f0969a
Egor <egorrachkov0@gmail.com>
2023-07-22 22:32:43 +0500

    Add logic for add products in storages

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,50 @@ public class Main {
                 new Product("Snickers", 500),
                 new Product("Mars", 450)
         ));
-        ArrayList<Storage> storages = new ArrayList<>();
+        ArrayList<Storage> storages = new ArrayList<>(Arrays.asList(
+                new Storage("Qwerty", "Moscow", 1001, products),
+                new Storage("ZXC", "Aktau", 1002, products)
+        ));
 
         Scanner enterLogin = new Scanner(System.in);
         System.out.println("Введите логин: ");
         String login = enterLogin.nextLine();
 
-        if(isAuth = login.equals("Admin")){
+        if (isAuth = login.equals("Admin")) {
             System.out.println("Что хотите сделать с товаром? Введите название одного из вариантов:\n 1.Добавить\n 2.Изменить");
             Scanner variant = new Scanner(System.in);
             String selectedVariant = variant.nextLine();
             System.out.println("Выбранный вариант: " + selectedVariant + " товар");
-        } else{
+
+            System.out.println("Введите название нужного склада: ");
+            Scanner selectStorageInput = new Scanner(System.in);
+            String selectedStorage = selectStorageInput.nextLine();
+
+            Storage foundStorage = findStorageByName(storages, selectedStorage);
+            System.out.println(foundStorage);
+
+            Scanner inputProductInfo = new Scanner(System.in);
+            System.out.println("Введите название нового продукта: ");
+            String productName = inputProductInfo.nextLine();
+            System.out.println("Введите цену нового продукта: ");
+            int productPrice = inputProductInfo.nextInt();
+            Product newProduct = new Product(productName, productPrice);
+
+            assert foundStorage != null;
+            foundStorage.addProductsInStorage(newProduct);
+
+            System.out.println("Новый список продуктов на этом складе: " + products);
+        } else {
             System.out.println("Список всех товаров и информации о них\n" + products);
         }
+    }
+
+    private static Storage findStorageByName(ArrayList<Storage> storages, String storageName) {
+        for (Storage storage : storages) {
+            if (storage.getStorageName().equals(storageName)) {
+                return storage;
+            }
+        }
+        return null;
     }
 }
--- src/Storage.java
+++ src/Storage.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
+
 public class Storage {
-    int storageName;
+    String storageName;
     String storageLocation;
     int storageId;
+    ArrayList<Product> products;
 
-    public Storage() {
+    public Storage(String storageName, String storageLocation, int storageId, ArrayList<Product> products) {
+        this.storageId = storageId;
+        this.storageLocation = storageLocation;
+        this.storageName = storageName;
+        this.products = products;
     }
 
-    public int getStorageName() {
+    public String getStorageName() {
         return storageName;
     }
 
@@ -16,5 +23,24 @@ public class Storage {
 
     public int getStorageId() {
         return storageId;
+    }
+
+    public void addProductsInStorage(Product product){
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "название склада = '" + storageName + '\'' +
+                ", расположение склада = '" + storageLocation + '\'' +
+                ", айди склада = " + storageId;
     }
 }
